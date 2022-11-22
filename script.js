ab85b4a6a48938a9e992a543f03b4da98c89340d
Miraj Ali <87961346+mirajalimolla@users.noreply.github.com>
2022-11-22 22:28:06 +0530

    Remove the audio
    Remove the audio

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 // Play the audio
-function play(){
-    let audio = new Audio("bgSound.mp3");
-    audio.play();
-}
 
 // This is a name chenger
 const url = window.location.search;
@@ -14,5 +10,4 @@ document.querySelector("#name").innerHTML = url_value;
 function showVideo(){
     document.querySelector("#container_all").style.display="none";
     document.querySelector("#viedo_section").style.display="block";
-    audio.pause();
 }
