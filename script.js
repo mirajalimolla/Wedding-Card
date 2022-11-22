2dc56031f12af011f096d0122b07bae111f840dd
Miraj Ali <87961346+mirajalimolla@users.noreply.github.com>
2022-11-22 16:48:33 +0530

    sssss
    ssssss

--- script.js
+++
@@ -1,8 +0,0 @@
-// var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
-//   audio.play();
-
-// This is a name chenger
-const url = window.location.search;
-let search = new URLSearchParams(url);
-let url_value = search.get("name");
-document.querySelector("#name").innerHTML = url_value;
