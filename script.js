05d31e6d8ed452b0a3adc7a3c81155852b0e264c
Miraj Ali <87961346+mirajalimolla@users.noreply.github.com>
2022-11-22 21:42:48 +0530

    Call Audio
    Call Audio

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+// Play the audio
+function play(){
+    let audio = new Audio("bgSound.mp3");
+    audio.play();
+}
 
 // This is a name chenger
 const url = window.location.search;
@@ -10,4 +14,5 @@ document.querySelector("#name").innerHTML = url_value;
 function showVideo(){
     document.querySelector("#container_all").style.display="none";
     document.querySelector("#viedo_section").style.display="block";
+    play();
 }
